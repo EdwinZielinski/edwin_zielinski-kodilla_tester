d6f90bf640948622414bcca7f838f2d69d99662b
Edwin Zielinski <edwinzielinski007@gmail.com>
2020-07-08 11:50:12 +0200

    MainExtanded

---
+++ kodilla-intro/src/main/java/MainExtanded.java
@@ -0,0 +1,11 @@
+public class MainExtanded {
+    public static void main(String[] args) {
+        String example = "Hello to my Mentor!";
+        int myAge = 31;
+        double height = 1.89;
+        char firstLetter = 'E';
+        boolean knowHowToKodeWell = false;
+        boolean wantToDoItWell = true;
+        System.out.println(example);
+    }
+}
