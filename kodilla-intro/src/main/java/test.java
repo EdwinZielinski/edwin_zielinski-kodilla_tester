e108d5918df5dfb167584ce7f3d543ce2d1c8090
Edwin Zielinski <edwinzielinski007@gmail.com>
2020-08-06 13:27:22 +0200

    Initial version

--- kodilla-intro/src/main/java/test.java
+++ kodilla-intro/src/main/java/test.java
@@ -1,2 +1,20 @@
-public class test {
+class contoAlfa
+{
+    String nazwa = "Tomek";
+    int wiek = 30;
+    double stanKonta = 3156.4;
+
+    void wypisz()
+    {
+        System.out.println("nazwa użytkownika " + nazwa);
+        System.out.println("wiek użytkownika " + wiek);
+        System.out.println("stan konta użytkownika " + stanKonta);
+    }
+}
+
+class main {
+    public static void main(String[] args) {
+        contoAlfa k1 = new contoAlfa();
+        k1.wypisz();
+    }
 }
