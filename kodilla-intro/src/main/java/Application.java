240665a43a585adcbb3b3da717d9e8efdba6cd65
Edwin Zielinski <edwinzielinski007@gmail.com>
2020-08-15 18:13:27 +0200

    FirstClass

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,16 @@
+public class Application {
+        public static void main(String[] args) {
+            String name = "Adam";
+            double age = 20.5;
+            int height = 178;
+
+
+                if (age > 30 && height > 160) {
+                    System.out.println(name + " " + "is older than 30 and higher then 160cm");
+                } else {
+                    System.out.println(name + " " + "User is younger than 30 or lower than 160cm");
+                }
+            }
+        }
+
+
