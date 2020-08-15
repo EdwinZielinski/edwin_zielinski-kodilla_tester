aee844f8e1a7397ce8da4d07fba6fcbe87b42750
Edwin Zielinski <edwinzielinski007@gmail.com>
2020-08-15 15:12:14 +0200

    Notebook

--- kodilla-intro/src/main/java/notebook.java
+++ kodilla-intro/src/main/java/notebook.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class notebook {
+class Notebook {
+    String weight;
+    int price;
+
+
+    public Notebook(String weight, int price) {
+        this.weight = weight;
+        this.price = price;
+
+    }
+
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap");
+        } else if (this.price >= 600 && this.price <= 1000) {
+            System.out.println("The price is good");
+        } else {
+            System.out.println("This notebook is expensive");
+        }
+
+
+    }
+    public void checkWeight() {
+        if (this.weight == "600g") {
+            System.out.println("This notebook is light");
+        }
+        else if (this.weight == "1600g") {
+            System.out.println("This notebook is not so heavy");
+        }
+        else {
+            System.out.println("This notebook is very heavy");
+        }
+    }
 }
