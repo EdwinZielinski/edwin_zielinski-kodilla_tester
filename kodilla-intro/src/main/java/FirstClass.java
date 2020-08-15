e8379214325cf78248af9f4f5e2681a5861d3dd8
Edwin Zielinski <edwinzielinski007@gmail.com>
2020-08-15 15:14:11 +0200

    FirstClass

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,18 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
+        Notebook notebook = new Notebook("600g", 1000);
+        System.out.println(notebook.weight+ " " + notebook.price);
+            notebook.checkPrice();
+            notebook.checkWeight();
+
+        Notebook heavyNotebook = new Notebook("2000g", 1500);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+            heavyNotebook.checkPrice();
+            heavyNotebook.checkWeight();
+
+        Notebook oldNotebook = new Notebook("1600g", 500);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+            oldNotebook.checkPrice();
+            oldNotebook.checkWeight();
     }
 }
