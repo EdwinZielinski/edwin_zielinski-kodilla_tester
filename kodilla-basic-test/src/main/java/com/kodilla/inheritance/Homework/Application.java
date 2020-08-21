eb80659b387ec103230ffa26babb063edd52b86a
Edwin Zielinski <edwinzielinski007@gmail.com>
2020-08-21 13:21:31 +0200

    FirstClass

---
+++ kodilla-basic-test/src/main/java/com/kodilla/inheritance/Homework/Application.java
@@ -0,0 +1,7 @@
+package com.kodilla.inheritance.Homework;
+
+public class Application {
+    public static void main(int[]args){
+        OperetingSystem operetingSystem = new OperetingSystem();
+    }
+}
